5f24ac17fa2a26f36df15d8b7327b6940fd16007
Rafazg <claudenirorafaelbjj@gmail.com>
2025-10-21 04:45:54 -0300

    feat: implements UserDetails and import SpringSecurity package

--- src/main/java/com/zgdev/aprendendo_spring/infrastructure/repository/UsuarioRepository.java
+++ src/main/java/com/zgdev/aprendendo_spring/infrastructure/repository/UsuarioRepository.java
@@ -2,8 +2,11 @@ package com.zgdev.aprendendo_spring.infrastructure.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.zgdev.aprendendo_spring.infrastructure.entity.Usuario;
+import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 
     boolean existsByEmail(String email);
+
+    Optional<Usuario> findByEmail(String email);
 }
